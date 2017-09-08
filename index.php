93b9b7dba5630a634466784829b39d3e84e267b9
jiangxujin <432853096@qq.com>
2017-09-08 15:24:01 +0800

    Signed-off-by: jiangxujin <432853096@qq.com>

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
     <li data-target="#carousel-generic" data-slide-to="0" class="active"></li>
     <li data-target="#carousel-generic" data-slide-to="1"></li>    
   </ol>
-
-  
   <div class="carousel-inner" role="listbox">
     <div class="item active">
       <img src="images/banner01.jpg" />
@@ -16,8 +14,6 @@
       <img src="images/banner02.jpg" />
     </div>
   </div>
-
-  
   <a class="left carousel-control" href="#carousel-generic" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
