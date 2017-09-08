82dc05df7eb31416c51282bd8f5f5d97c71d43e5
jiangxujin <432853096@qq.com>
2017-09-08 10:24:41 +0800

    Signed-off-by: jiangxujin <432853096@qq.com>

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html lang="zh-cn">
-
 <head>
 	<title>慧人事后台管理中心</title>
 	<meta charset="utf-8">
@@ -15,69 +14,31 @@
   <![endif]-->
 
 </head>
-
 <body>
 <!--header-->
-<header>
-	<div class="headerpanel">
-		<h2 class="pull-left">麦智云服人力资源云管理平台</h2>
-		<div class="headerbar">
-			<div class="header-right">
-				<ul class="headermenu">
-					<li>
-						<div class="btn-group">
-							<button type="button" class="btn btn-logged" data-toggle="dropdown">
-								<img width="40" src="images/user-header.jpg" alt="" />
-								张三（技术总监）<span class="caret"></span>
-							</button>
-							<ul class="dropdown-menu pull-right fadeIn animated">
-								<li><a href="zhang_hao_she_zhi.php">账号设置</a></li>
-								<li><a href="qi_ye_ren_zheng.php">企业认证</a></li>
-								<li><a href="#">安全退出</a></li>
-							</ul>
-						</div>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand"><img src="images/logo.svg" /></a>
+				<ul class="nav navbar-nav">
+					<li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">东莞 <span class="caret"></span></a>
+					  <ul class="dropdown-menu">						
+						<li><a href="#">北京</a></li>
+						<li><a href="#">临忻</a></li>
+						<li><a href="#">烟台</a></li>
+						<li><a href="#">西安</a></li>
+					  </ul>
 					</li>
-					<li>
-						<div class="btn-group">
-							<button type="button" class="btn btn-logged" data-toggle="dropdown"> 
-								<i class="fa fa-bell"></i><sup><span class="badge badge-important">2</span></sup> 
-							</button>
-							<div class="dropdown-menu dropdown-menu-message pull-right fadeIn animated">
-							  <div class="panel">
-								<div class="panel-body">
-								  <ul class="list-group">
-									  	<li class="list-group-item"> <a>29件退保未处理29件退保未处理</a><span class="badge">2016-01-02</span> </li>
-										<li class="list-group-item"> <a>29件退保未处理</a><span class="badge">2016-01-02</span> </li>
-										<li class="list-group-item"> <a>29件退保未处理</a><span class="badge">2016-01-02</span> </li>
-								  </ul>
-								  <hr/>
-								  <a class="center-block text-center mb10" href="#">查看更多</a> </div>
-							  </div>
-							</div>
-						</div>
-					</li>
-					<li>
-					<div class="btn-group">
-						<button type="button" class="btn btn-logged" data-toggle="dropdown"> 
-							<i class=" icon-zhichi iconfont"></i> 
-						</button>
-						<div class="dropdown-menu dropdown-menu-aboutus pull-right fadeIn animated">
-					  		<div class="panel">
-								<div class="panel-body nopaddingbottom">
-								  <div class="col-xs-5"><img width="100" src="images/user-header.jpg" /></div>
-								  <div class="col-xs-7">
-									<h4>技术支持</h4>
-									  <p><i class="iconfont icon-Phone"></i>&nbsp;400-626-6650</p>
-									  <p><i class="iconfont icon-QQ"></i>&nbsp;2469891294</p>
-								  </div>
-							  </div>
-							</div>
-						</div>
-					</div>
-					</li>
+      			</ul>
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="#">内行</a></li>
+					<li><a href="#">新房</a></li>
+					<li><a href="#">房产资讯</a></li>
 				</ul>
 			</div>
 		</div>
-	</div>
-</header>
+	</nav>
 <!--header end-->
