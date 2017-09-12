6cdb95f3052f112a65f77f3654e84c6636640622
jiangxujin <432853096@qq.com>
2017-09-12 13:09:55 +0800

    Signed-off-by: jiangxujin <432853096@qq.com>

--- index.php
+++ index.php
@@ -1,117 +1,243 @@
-﻿<?php include 'header.php';?> 
+﻿<?php include 'header.php';?>
 <!--banner-->
 <div id="carousel-generic" class="carousel slide  hidden-xs" data-ride="carousel">
 	<div class="count-box animated fadeInLeftBig fadeIn">
 		<h3>议房神算</h3>
 		<form>
 			<div class="input-group input-group-sm">
-			  <span class="input-group-btn ">
+				<span class="input-group-btn ">
 				<span class="btn btn-default">小区名称</span>
-			  </span>
-			  <input type="text" class="form-control" placeholder="">
+			
+				</span>
+				<input type="text" class="form-control" placeholder="">
 			</div>
 			<div class="input-group input-group-sm">
-			  <span class="input-group-btn">
+				<span class="input-group-btn">
 				<span class="btn btn-default">户型</span>
-			  </span>
+			
+				</span>
 				<select class="form-control">
 					<option>大户型</option>
 				</select>
 			</div>
 			<div class="input-group input-group-sm">
-			  <span class="input-group-btn">
+				<span class="input-group-btn">
 				<span class="btn btn-default">面积</span>
-			  </span>
-			  <input type="text" class="form-control" placeholder="">
+			
+				</span>
+				<input type="text" class="form-control" placeholder="">
 			</div>
 			<div class="input-group input-group-sm">
-			  <span class="input-group-btn">
+				<span class="input-group-btn">
 				<span class="btn btn-default">楼层</span>
-			  </span>
-			  <input type="text" class="form-control" placeholder="">
+			
+				</span>
+				<input type="text" class="form-control" placeholder="">
 			</div>
 			<button type="button" class="btn btn-primary btn-block">优惠计算</button>
 		</form>
 	</div>
-	
+
 	<ol class="carousel-indicators">
 		<li data-target="#carousel-generic" data-slide-to="0" class="active"></li>
-		<li data-target="#carousel-generic" data-slide-to="1"></li>    
+		<li data-target="#carousel-generic" data-slide-to="1"></li>
 	</ol>
 	<div class="carousel-inner" role="listbox">
 		<div class="item active">
-		  <img src="images/banner01.jpg" />
+			<img src="images/banner01.jpg"/>
 		</div>
 		<div class="item">
-		  <img src="images/banner02.jpg" />
+			<img src="images/banner02.jpg"/>
 		</div>
 	</div>
 	<a class="left carousel-control" href="#carousel-generic" role="button" data-slide="prev">
 		<span class="iconfont icon-left" aria-hidden="true"></span>
 		<span class="sr-only">Previous</span>
 	</a>
+
 	<a class="right carousel-control" href="#carousel-generic" role="button" data-slide="next">
 		<span class="iconfont icon-right" aria-hidden="true"></span>
 		<span class="sr-only">Next</span>
 	</a>
+
 </div>
 <!--banner end-->
 <div class="container home01">
 	<div class="col-xs-02" style="margin-left: 0;">
-		<img src="images/home-icon-01.png" />
+		<img class="animated fadeIn" src="images/home-icon-01.png"/>
 		<p>议房头条</p>
 	</div>
 	<div class="col-xs-02">
-		<img src="images/home-icon-02.png" />
+		<img class="animated fadeIn" src="images/home-icon-02.png"/>
 		<p>百答大咖</p>
 	</div>
 	<div class="col-xs-02">
-		<img src="images/home-icon-03.png" />
+		<img class="animated fadeIn" src="images/home-icon-03.png"/>
 		<p>智能配房</p>
 	</div>
 	<div class="col-xs-02">
-		<img src="images/home-icon-04.png" />
+		<img class="animated fadeIn" src="images/home-icon-04.png"/>
 		<p>议房神算</p>
 	</div>
 	<div class="col-xs-02">
-		<img src="images/home-icon-05.png" />
+		<img class="animated fadeIn" src="images/home-icon-05.png"/>
 		<p>新房推荐</p>
 	</div>
 	<div class="clearfix"></div>
-	<br />
-	<br />
-	<div class="pull-left hidden-xs" >
+	<br/>
+	<br/>
+	<div class="pull-left hidden-xs">
 		<div class="h3 mt5">百答大咖</div>
 	</div>
 	<div class="pull-right hidden-xs">
-		<form class="pull-left">			
+		<form class="pull-left form-inline">
 			<div class="input-group input-group-sm">
-				<input class="form-control input-sm" />
+				<input class="form-control input-sm"/>
 				<div class="input-group-btn">
-					<button class="btn btn-default btn-sm" style="min-height: 0;">搜索</button>
+					<button class="btn btn-default btn-sm" style="min-height:0;">搜索</button>
 				</div>
 			</div>
 		</form>
 		<a class="pull-right mt5 ml10 size14 text-333" href="#">查看更多>></a>
 	</div>
 </div>
-<br />
+<br/>
 <div class="container-fluid nopadding">
-	<img width="100%" src="images/test1.png" />
+	<img width="100%" style="min-height: 100px;" src="images/test1.png"/>
 </div>
-<?php include 'footer.php';?>
+<div class="container home02 hidden-xs">
+	<div class="col-xs-3">
+		<div class="thumbnail">
+			<a href="#">
+				<img src="images/test02.png"/>
+				<div class="caption">
+					<h4>服务说明</h4>
+					<p>从买房者的利益出发</p>
+					<p>10年以上经验的大咖</p>
+					<p>资深的房产买卖视角</p>
+					<p>不满意就退款的保障</p>
+				</div>
+			</a>
+		</div>
+		<div class="home02-news">
+		  <ul class="nav nav-tabs" role="tablist">
+			<li role="presentation" class="active"><a href="#item1"  role="tab" data-toggle="tab">最新动态</a></li>
+			<li role="presentation"><a href="#item2" role="tab" data-toggle="tab">最新评价</a></li>			
+		  </ul>
+		  <div class="tab-content">
+			<div role="tabpanel" class="tab-pane active" id="item1">
+				
+			</div>
+			<div role="tabpanel" class="tab-pane" id="item2">
+				
+			</div>			
+		  </div>
+		</div>
+	</div>
+	<div class="col-xs-9">
+		<div class="col-xs-4">
+			<div class="thumbnail">
+				<a href="#">
+					<img src="images/test04.png"/>
+					<div class="caption">
+						<h4>张明利</h4>
+						<ul class="list-unstyled">
+							<li>从事房产产业13年</li>
+							<li>10年以上房产大咖</li>
+							<li>业务范围：租房、卖房、租房、卖房、租房、卖房</li>
+						</ul>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<div class="thumbnail">
+				<a href="#">
+					<img src="images/test02.png"/>
+					<div class="caption">
+						<h4>李明华</h4>
+						<ul class="list-unstyled">
+							<li>从事房产产业13年</li>
+							<li>10年以上房产大咖</li>
+							<li>业务范围：租房、卖房、租房、卖房、租房、卖房</li>
+						</ul>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<div class="thumbnail">
+				<img src="images/test04.png"/>
+				<div class="caption">
+					<h4>李天一</h4>
+					<ul class="list-unstyled">
+						<li>从事房产产业13年</li>
+						<li>10年以上房产大咖</li>
+						<li>业务范围：租房、卖房、租房、卖房、租房、卖房</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<div class="thumbnail">
+				<img src="images/test02.png"/>
+				<div class="caption">
+					<h4>胡春春</h4>
+					<ul class="list-unstyled">
+						<li>从事房产产业13年</li>
+						<li>10年以上房产大咖</li>
+						<li>业务范围：租房、卖房、租房、卖房、租房、卖房</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<div class="thumbnail">
+				<img src="images/test04.png"/>
+				<div class="caption">
+					<h4>东方不败</h4>
+					<ul class="list-unstyled">
+						<li>从事房产产业13年</li>
+						<li>10年以上房产大咖</li>
+						<li>业务范围：租房、卖房、租房、卖房、租房、卖房</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<div class="thumbnail">
+				<img src="images/test02.png"/>
+				<div class="caption">
+					<h4>李宁</h4>
+					<ul class="list-unstyled">
+						<li>从事房产产业13年</li>
+						<li>10年以上房产大咖</li>
+						<li>业务范围：租房、卖房、租房、卖房、租房、卖房</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div> 
+ <?php include 'footer.php';?>
 <script>
-	$(function(){
+	$( function () {
 		//计算BOX距离
-        $(window).resize(function () {
-            var left = ($(window).width() - $('.container').width()) / 2;
-			var top = ($('.carousel').height() - $('.count-box').height())/2
-            $('.count-box').css('left', left);
-			$('.count-box').css('top', top);
-        })		
-		var top = ($('.carousel').height() - $('.count-box').height())/2
-        var left = ($(window).width() - $('.container').width()) / 2;
-        $('.count-box').css('left', left);
-		$('.count-box').css('top', top);
-	})
+		$( window ).resize( function () {
+			var left = ( $( window ).width() - $( '.container' ).width() ) / 2;
+			var top = ( $( '.carousel' ).height() - $( '.count-box' ).height() ) / 2
+			$( '.count-box' ).css( 'left', left );
+			$( '.count-box' ).css( 'top', top );
+		} )
+		var top = ( $( '.carousel' ).height() - $( '.count-box' ).height() ) / 2
+		var left = ( $( window ).width() - $( '.container' ).width() ) / 2;
+		$( '.count-box' ).css( 'left', left );
+		$( '.count-box' ).css( 'top', top );
+		//房产大咖
+		$( '.thumbnail' ).hover(function (){			
+			$(this).find('.caption').css('bottom','0');
+		},function(){
+			$(this).find('.caption').css('bottom','-31%');
+		})
+	} )
 </script>
